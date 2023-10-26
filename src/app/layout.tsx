cff89bb398a7343dd6e1eba6c4580884aa4ac4c4
Oleh Panchenko <106483317+opanchen@users.noreply.github.com>
2023-10-26 23:03:31 +0200

    created dynamic favicon
    - implemented dynamic favicon icon according to system theme-mode  via metadata code inside layout.tsx component;
    - removed useless svg-files gotten from initial project template;

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,22 @@ type Props = {
 export const metadata: Metadata = {
   title: "CarpTravel",
   description: "Uncover Carpathian's secrets",
+  icons: {
+    icon: [
+      {
+        rel: "icon",
+        media: "(prefers-color-scheme: light)",
+        url: "/assets/favicon/icon-light.png",
+        href: "/assets/favicon/icon-light.png",
+      },
+      {
+        rel: "icon",
+        media: "(prefers-color-scheme: dark)",
+        url: "/assets/favicon/icon-dark.png",
+        href: "/assets/favicon/icon-dark.png",
+      },
+    ],
+  },
 };
 
 const RootLayout = ({ children }: Props) => {
